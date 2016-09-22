d5e356f335efe01069b0272838264410dfb235b8
Marek Hrusovsky <xhruso00@gmail.com>
2016-09-22 12:43:45 +0200

    Added controller

---
+++ badge/BadgeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BadgeViewController.h
+//  badge
+//
+//  Created by Marek Hrusovsky on 9/22/16.
+//  Copyright © 2016 Marek Hrusovsky. All rights reserved.
+//
+
+#import "MyViewController.h"
+
+@interface BadgeViewController : MyViewController
+
+@end
---
+++ badge/BadgeViewController.m
@@ -0,0 +1,22 @@
+//
+//  BadgeViewController.m
+//  badge
+//
+//  Created by Marek Hrusovsky on 9/22/16.
+//  Copyright © 2016 Marek Hrusovsky. All rights reserved.
+//
+
+#import "BadgeViewController.h"
+
+@interface BadgeViewController ()
+
+@end
+
+@implementation BadgeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
